6da431ddcf333a2ce3f5b12cdf1886915e8762a9
JustHolyCow <79006907+JustHolyCow@users.noreply.github.com>
2021-06-08 12:12:10 +0500

    Update app.js

--- labs/1/app.js
+++ labs/1/app.js
@@ -1,65 +1,33 @@
-let a = Number(prompt("Введите A:"));
-let b = Number(prompt("Введите B:"));
-let c = Number(prompt("Введите C:"));
-/*if (isNaN(a) || isNaN(b) || isNaN(c))
-{
-	console.log("Error NaN");
-}
-if*/
-// 1) Проверить, что введены числа
-// 2) определить по коэффициентам что именно перед нами (квадратное уравнение,
-//   уравнение первой степени или правильное или неправильное равенство)
-if ((isFinite(a) && isFinite(b) && isFinite(c)))
-{
-	if (a === 0 && b === 0) 
-	{
-		if (c === 0)
-		{
-			alert("0 = 0");
-		} 
-		else 
-		{
-			alert("Неверное равенство " + c + " = 0");
-		}
-	}
-	else if (a === 0)
-	{
-		let x;
-		x = -c/b;
-		x = Number(x.toFixed(2));
-		alert(x);
-	} 
-	else 
-	{
-		let x1;
-		let x2;
-		let D;
-		D = b**2 - 4 * a * c;
-		if (D >= 0)
-		{
-			x1 = (-b + Math.sqrt(D))/2;
-			x2 = (-b - Math.sqrt(D))/2;
-			x1 = Number(x1.toFixed(2));
-			x2 = Number(x2.toFixed(2));
+function math(){
+	let a = prompt("Введите коэффициент A",);
+	if(a == 0){
+		let b = prompt("Введите коэффициент B",);
+		let c = prompt("Введите коэффициент C",); 
+		if(b == 0){
+			if(c == 0)
+				alert("0 == 0");
+			else
+				alert(" С != 0");
 		}
 		else
-		{
-			x1 = -b/2;
-			x2 = -b/2;
-			x1 = String(x1.toFixed(2));
-			x2 = String(x2.toFixed(2));
-			x1 += ' ' + '+';
-			x2 += ' ' + '-';
-			D = Math.sqrt(-D)/2;
-			D = String(D.toFixed(2));
-			D += 'i';
-			x1 += ' ' + D;
-			x2 += ' ' + D;
-		}
-		alert(x1 + " / " + x2);
+			alert("Корень уравнения " + ((-c)/b));
 	}
+	else{
+		let b = prompt("Введите коэффициент при B",);
+		let c = prompt("Введите коэффициент при C",); 
+		alert("Дискриминант " + (b*b-4*a*c));
+		if((b*b-4*a*c) < 0){
+			alert("Первый корень уравнения " + (-b/ 2*a)+ " + " + (((Math.sqrt(Math.abs(b*b-4*a*c))/ 2*a).toFixed(2) + "i")));
+			alert("Второй корень уравнения " + (-b/ 2*a) + " - " + (((Math.sqrt(Math.abs(b*b-4*a*c))/ 2*a).toFixed(2) + "i")));
+		}
+		else if((b*b-4*a*c) == 0){
+			alert("Корень уравнения " + ((-b)/ 2*a));
+		}
+		else{
+			alert("Первый корень уравнения " + (-b + (Math.sqrt(b*b-4*a*c))/ 2*a).toFixed(2));
+			alert("Второй корень уравнения " + (-b - (Math.sqrt(b*b-4*a*c))/ 2*a).toFixed(2));
+		}
+	}
+	return a;
 }
-else
-{
-	alert("Неверно введены числа");
-}
+let sol = math();
